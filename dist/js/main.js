1cb50cdf9c9c6195532d0b8c940f4a65b045891d
Erik Olson <erik@freshims.com>
2014-07-04 17:12:04 -0400

    Updated Erik's bio, added 757 to the list list of names our area goes by.

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,21 @@
-!function(console,document,location,$){"use strict";$("[data-toggle='tooltip']").tooltip();var reason,reasonDescription,thanksBar=$("#thanksBar"),registerBar=$("#registerBar"),areaNames=["Norfolk","Virginia Beach","Hampton Roads","Chesapeake","Newport News","Coastal Virginia","Portsmouth","Suffolk","Hampton","Tidewater"],currentArea=0;setInterval(function(){var areaBox=$("#themtoo"),randomArea=areaNames[currentArea];currentArea+=1,currentArea>areaNames.length-1&&(currentArea=0),areaBox.fadeOut(function(){areaBox.text(randomArea),areaBox.fadeIn()})},2e3),thanksBar.hide().removeClass("hide"),registerBar.hide().removeClass("hide"),"?thanks"===location.search.toLowerCase()?(thanksBar.show(),registerBar.hide()):(thanksBar.hide(),registerBar.show()),location.search.toLowerCase().indexOf("?error")>=0&&(reason=location.search.toLowerCase().split("=")[1],reasonDescription="A wicked error occured.",reason.toLowerCase()==="List_AlreadySubscribed".toLowerCase()&&(reasonDescription="No need to fear!  You're already on the list.  We'll keep you posted."),reason.toLowerCase()==="ValidationError".toLowerCase()&&(reasonDescription="We need an email address, not a random collection of characters.  Try again."),$("#registerEmailAddress").popover({title:"Whoa.",content:reasonDescription,trigger:"manual",placement:"top"}).popover("toggle")),$("#registerEmailAddress").focus()}(console,document,location,$);
+!function(console, document, location, $) {
+    "use strict";
+    $("[data-toggle='tooltip']").tooltip();
+    var reason, reasonDescription, thanksBar = $("#thanksBar"), registerBar = $("#registerBar"), areaNames = [ "Norfolk", "Virginia Beach", "Hampton Roads", "Chesapeake", "Newport News", "Coastal Virginia", "Portsmouth", "Suffolk", "Hampton", "757", "Tidewater" ], currentArea = 0;
+    setInterval(function() {
+        var areaBox = $("#themtoo"), randomArea = areaNames[currentArea];
+        currentArea += 1, currentArea > areaNames.length - 1 && (currentArea = 0), areaBox.fadeOut(function() {
+            areaBox.text(randomArea), areaBox.fadeIn();
+        });
+    }, 2e3), thanksBar.hide().removeClass("hide"), registerBar.hide().removeClass("hide"), 
+    "?thanks" === location.search.toLowerCase() ? (thanksBar.show(), registerBar.hide()) : (thanksBar.hide(), 
+    registerBar.show()), location.search.toLowerCase().indexOf("?error") >= 0 && (reason = location.search.toLowerCase().split("=")[1], 
+    reasonDescription = "A wicked error occured.", reason.toLowerCase() === "List_AlreadySubscribed".toLowerCase() && (reasonDescription = "No need to fear!  You're already on the list.  We'll keep you posted."), 
+    reason.toLowerCase() === "ValidationError".toLowerCase() && (reasonDescription = "We need an email address, not a random collection of characters.  Try again."), 
+    $("#registerEmailAddress").popover({
+        title: "Whoa.",
+        content: reasonDescription,
+        trigger: "manual",
+        placement: "top"
+    }).popover("toggle")), $("#registerEmailAddress").focus();
+}(console, document, location, $);
